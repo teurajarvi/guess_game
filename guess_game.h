6715acb5023db444917115424d940d8e6c636e95
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2016-07-26 13:35:47 +0300

    Create guess_game.h
    Initial .h header file for Guess game (language Finnish)

---
+++ guess_game.h
@@ -0,0 +1,14 @@
+/* oma header: guess_game.h */
+#include <stdbool.h> /* C doesn't have build in boolean */
+
+/* muuttujat */
+#define FALSE 0
+#define TRUE 1
+
+/* tyypit */
+#define LUKUVIRHE 9999
+
+/* funktion määritelmät */
+int arvonta( );
+bool tarkista_luku( );
+void anna_vihje( );
